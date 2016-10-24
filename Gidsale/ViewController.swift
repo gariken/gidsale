fdd28e29cd99a93b14cc5f662739604c42d4409b
Alexander Goryatov <schet941@gmail.com>
2016-10-24 14:07:38 +0300

    first

---
+++ Gidsale/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Gidsale
+//
+//  Created by Александр on 20.10.16.
+//  Copyright © 2016 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+  
+
+
+}
+
